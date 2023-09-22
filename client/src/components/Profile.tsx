2df35d3a92fdd0e4971bca7a71f0c8bffcecbfa8
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-09-22 09:26:11 +0200

    white

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -7,8 +7,8 @@ export const Profile = () => {
         const { avatar, firstName, lastName } = loggedUser;
         return (
             <div className='w-full h-14 py-2 my-auto text-center border border-black bg-black absolute bottom-0'>
-                <div className="w-12 h-12"><img src={avatar}/></div>
-                <div>{firstName} {lastName}</div>
+                <div className="w-12 h-12 ml-3"><img src={avatar}/></div>
+                <div className="text-white">{firstName} {lastName}</div>
                 <div
                     className="w-5 h-5 absolute right-5 bottom-5 cursor-pointer"
                     onClick={handleModalProfile}
