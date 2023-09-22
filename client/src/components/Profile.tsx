7c3e9c7fee91606d04b56b46fb6df4aed0bfbe5e
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-09-22 08:44:48 +0200

    profilphoto

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -7,7 +7,7 @@ export const Profile = () => {
         const { avatar, firstName, lastName } = loggedUser;
         return (
             <div className='w-full h-14 py-2 my-auto text-center border border-black bg-black absolute bottom-0'>
-                <div><img src={avatar}/></div>
+                <div className="w-5 h-5"><img src={avatar}/></div>
                 <div>{firstName} {lastName}</div>
                 <div
                     className="w-5 h-5 absolute right-5 bottom-5 cursor-pointer"
@@ -30,7 +30,7 @@ export const Profile = () => {
                 className="w-5 h-5 absolute right-5 bottom-5 cursor-pointer"
                 onClick={handleModalProfile}
             >
-                <img src='/arrow_down.svg' alt="arrow_down" />
+                <img src='/arrow_down.svg' alt="arrow_down"/>
             </div>
         </div>
     )
